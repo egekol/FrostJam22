1a12921844664213faf909a4bd2341490db78819
egekol <kol18@itu.edu.tr>
2022-02-12 19:57:49 +0300

    Project Init

---
+++ Assets/Scripts/GamePlay/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("Horizontal: " + floatingJoystick.Horizontal);
+        // var camPos = cam.transform.forward;
+        // camPos.y = 0;
+        // direction = ( floatingJoystick.Horizontal *cam.transform.right) +
+        //             (  floatingJoystick.Vertical *camPos);
+        // direction = Vector3.forward * floatingJoystick.Vertical + Vector3.right * floatingJoystick.Horizontal;
+        // direction = direction.normalized;
+        // direction.x = direction.x * Mathf.Abs(floatingJoystick.Horizontal);
+        // direction.z = direction.z * Mathf.Abs(floatingJoystick.Vertical);
+        // direction = direction.normalized;
+        // direction.y = 0;
+   
+    }
+}
